279019c21850049e7e8bf0570073d23fd3a2fdae
andreanwill <164478603+andreanwill@users.noreply.github.com>
2024-06-09 00:00:22 +0700

    Form Mata Kuliah
    Form Matakuliah Praktek PBO

---
+++ matakuliah/model/MataKuliahDao.java
@@ -0,0 +1,41 @@
+
+package matakuliah.model;
+import matakuliah.mataKuliah.MataKuliah;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MataKuliahDao {
+    private List<MataKuliah> data = new ArrayList<MataKuliah>();
+    
+    public MataKuliahDao()
+    {
+        data.add(new MataKuliah("Budi","2013125467","4","Akuntansi","2A","Akuntansi Pengantar","3"));
+        data.add(new MataKuliah("Mamat","2012432104","4","Elektro","2D","Sistem Kontrol Elektronika","5"));
+        data.add(new MataKuliah("Siti","2312954392","2","Bahasa Inggris","1C","Vocabulary Building","4"));
+    }
+    
+    public void insert(MataKuliah m)
+    {
+        data.add(m);
+    }
+    
+    public void update(int index, MataKuliah m)
+    {
+        data.set(index, m);
+    }
+    
+    public void delete(int index)
+    {
+        data.remove(index);
+    }
+
+    public MataKuliah getMataKuliah(int index)
+    {
+        return data.get(index);
+    }
+
+    public List<MataKuliah> getAllMataKuliah() 
+    {
+        return data;
+    }
+}
