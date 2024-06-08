bc4c2c46cb60b93b429793fe0fa1ec033966d169
andreanwill <164478603+andreanwill@users.noreply.github.com>
2024-06-08 23:53:08 +0700

    Form Mahasiswa
    Form Mahasiswa Praktek PBO

---
+++ mahasiswa/controler/MahasiswaControler.java
@@ -0,0 +1,107 @@
+
+package mahasiswa.controler;
+
+import mahasiswa.Mahasiswa;
+import mahasiswa.model.FormMahasiswa;
+import mahasiswa.model.MahasiswaDao;
+import java.util.HashSet;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+public class MahasiswaControler 
+{
+    FormMahasiswa formMahasiswa;
+    Mahasiswa mahasiswa;
+    MahasiswaDao mahasiswaDao;
+    int index;
+    
+    public MahasiswaControler(FormMahasiswa formMahasiswa)
+    {
+        this.formMahasiswa = formMahasiswa;
+        mahasiswaDao = new MahasiswaDao();
+    }
+    
+    public void cancel()
+    {
+        formMahasiswa.getTxtNOBP().setText("");
+        formMahasiswa.getTxtNama().setText("");
+        formMahasiswa.getTxtAlamat().setText("");
+        formMahasiswa.getTxtTglLahir().setText("");
+        isiJenisKelamin();
+    }
+    
+    public void isiJenisKelamin()
+    {
+        formMahasiswa.getCboJenisKelamin().removeAllItems();
+        formMahasiswa.getCboJenisKelamin().addItem("L");
+        formMahasiswa.getCboJenisKelamin().addItem("P");
+    }
+    
+    public void insert()
+    {
+        mahasiswa = new Mahasiswa();
+        mahasiswa.setNobp(formMahasiswa.getTxtNOBP().getText());
+        mahasiswa.setNama(formMahasiswa.getTxtNama().getText());
+        mahasiswa.setAlamat(formMahasiswa.getTxtAlamat().getText());
+        mahasiswa.setJenisKelamin(formMahasiswa.getCboJenisKelamin().getSelectedItem().toString());
+        mahasiswa.setTglalhir(formMahasiswa.getTxtTglLahir().getText());
+        mahasiswaDao.insert(mahasiswa);
+        JOptionPane.showMessageDialog(formMahasiswa, "Entry Data Ok");
+    }
+    
+    public void viewData()
+    {
+        DefaultTableModel model = (DefaultTableModel)formMahasiswa.getTabelMahasiswa().getModel();
+        model.setNumRows(0);
+        List<Mahasiswa> list = mahasiswaDao.getAllMahasiswa();
+        for(Mahasiswa mahasiswa :list)
+        {
+            Object[] data = 
+            {
+                mahasiswa.getNobp(),
+                mahasiswa.getNama(),
+                mahasiswa.getAlamat(),
+                mahasiswa.getJenisKelamin(),
+                mahasiswa.getTglalhir()
+            };
+            model.addRow(data);
+        }
+    }
+    
+    public void getMahasiswa()
+    {
+        int index = formMahasiswa.getTabelMahasiswa().getSelectedRow();
+        mahasiswa =  mahasiswaDao.getMahasiswa(index);
+        if (mahasiswa != null)
+        {
+            formMahasiswa.getTxtNOBP().setText(mahasiswa.getNobp());
+            formMahasiswa.getTxtNama().setText(mahasiswa.getNama());
+            formMahasiswa.getTxtAlamat().setText(mahasiswa.getAlamat());
+            formMahasiswa.getCboJenisKelamin().setSelectedItem(mahasiswa.getJenisKelamin());
+            formMahasiswa.getTxtTglLahir().setText(mahasiswa.getTglalhir());
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(formMahasiswa, "Mahasiswa Tidak Ada");
+        }
+    }
+    
+    public void update()
+    {
+        mahasiswa.setNobp(formMahasiswa.getTxtNOBP().getText());
+        mahasiswa.setNama(formMahasiswa.getTxtNama().getText());
+        mahasiswa.setAlamat(formMahasiswa.getTxtAlamat().getText());
+        mahasiswa.setJenisKelamin(formMahasiswa.getCboJenisKelamin().getSelectedItem().toString());
+        mahasiswa.setTglalhir(formMahasiswa.getTxtTglLahir().getText());
+        JOptionPane.showMessageDialog(formMahasiswa, "Update Data OK");
+    }
+    
+    public void delete()
+        {
+            mahasiswaDao.delete(index);
+            JOptionPane.showMessageDialog(formMahasiswa, "Delete Data Ok");
+        }
+    
+    
+}
