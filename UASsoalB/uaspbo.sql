dd14339c6e71e287256ca5b22632d7641ffe3abf
andreanwill <164478603+andreanwill@users.noreply.github.com>
2024-07-12 20:48:13 +0700

    Add files via upload

---
+++ UASsoalB/uaspbo.sql
@@ -0,0 +1,57 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: uaspbo
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `taxi`
+--
+
+DROP TABLE IF EXISTS `taxi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `taxi` (
+  `kodetransaksi` varchar(8) NOT NULL,
+  `kodepenumpang` varchar(8) NOT NULL,
+  `nama` varchar(45) NOT NULL,
+  `jenispenumpang` varchar(13) NOT NULL,
+  `platnomor` varchar(10) NOT NULL,
+  `supir` varchar(45) NOT NULL,
+  `biayaAwal` int(15) NOT NULL,
+  `biayaperkilo` int(15) NOT NULL,
+  `totalbayar` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `taxi`
+--
+
+LOCK TABLES `taxi` WRITE;
+/*!40000 ALTER TABLE `taxi` DISABLE KEYS */;
+INSERT INTO `taxi` VALUES ('20240712','A1001','Kontol','Langganan','BA696TOT','Supirman',120000,80000,200000),('2024bbb','bbb24','memek','Non-Langganan','BH6969TOT','Titit',125000,75000,200000);
+/*!40000 ALTER TABLE `taxi` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-12 20:43:48
